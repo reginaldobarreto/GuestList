11e81e8812b0a7041980009a8fa87de056d06d66
Reginaldo Barreto <reginaldo.barreto@gmail.com>
2019-12-31 01:36:20 -0300

    add fragment present

--- app/src/main/java/br/com/barrsoft/guestlist/fragments/AbsentFragment.java
+++ app/src/main/java/br/com/barrsoft/guestlist/fragments/AbsentFragment.java
@@ -1,4 +1,4 @@
-package br.com.barrsoft.guestlist;
+package br.com.barrsoft.guestlist.fragments;
 
 
 import android.os.Bundle;
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import br.com.barrsoft.guestlist.R;
 
 
 /**
